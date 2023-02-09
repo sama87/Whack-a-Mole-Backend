e1768762262757a7c7c285f463c230aa04874119
Sam Trimble <samuel.trimble87@gmail.com>
2023-02-09 13:49:48 -0600

    Initial SpringBoot Project

---
+++ MoleSpringBackend/src/main/java/com/cognixia/jump/MoleSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoleSpringBackendApplication.class, args);
+	}
+
+}
